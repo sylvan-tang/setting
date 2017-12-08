ca864e45b0d7a8f0a8e53b102e4049e4f81695b7
Sylvan <suna@zhihu.com>
2017-12-08 10:14:53 +0800

    Add develop tool settings

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
+#!/usr/bin/env bash
+user_name=$1
+password=$2
 ln -sf $(pwd)/config/profile ~/.profile
 ln -sf $(pwd)/config/bashrc ~/.bashrc
 ln -sf $(pwd)/config/git-completion.bash ~/git-completion.bash
+ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
+ln -sf $(pwd)/sbt/repositories ~/.sbt/repositories
+ln -sf $(pwd)/ssh/config ~/.ssh/config
+
+sed -i '' "s/LdapUserName/${user_name}/g" ivy2/.credentials
+sed -i '' "s/LdapUserPassword/${password}/g" ivy2/.credentials
+cp $(pwd)/ivy2/.credentials ~/.ivy2/.credentials
+
+sed -i '' "s/LdapUserName/${user_name}/g" m2/settings.xml
+sed -i '' "s/LdapUserPassword/${password}/g" m2/settings.xml
+cp $(pwd)/m2/settings.xml ~/.m2/settings.xml
+
+git add .
+git stash
