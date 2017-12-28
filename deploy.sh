8e8f7dff39b64f7499bf43da06c28118df1395e2
Sylvan <suna@zhihu.com>
2017-12-28 08:16:37 +0800

    Mkdir when folder is not exists

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,18 @@ ln -sf $(pwd)/config/profile ~/.profile
 ln -sf $(pwd)/config/bashrc ~/.bashrc
 ln -sf $(pwd)/config/git-completion.bash ~/git-completion.bash
 ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
+mkdir ~/.sbt
 ln -sf $(pwd)/sbt/repositories ~/.sbt/repositories
+mkdir ~/.ssh
 ln -sf $(pwd)/ssh/config ~/.ssh/config
 ln -sf $(pwd)/azkaban/.azkabanrc ~/.azkabanrc
 
+mkdir ~/.ivy2
 sed -i '' "s/LdapUserName/${user_name}/g" ivy2/.credentials
 sed -i '' "s/LdapUserPassword/${password}/g" ivy2/.credentials
 cp $(pwd)/ivy2/.credentials ~/.ivy2/.credentials
 
+mkdir ~/.m2/
 sed -i '' "s/LdapUserName/${user_name}/g" m2/settings.xml
 sed -i '' "s/LdapUserPassword/${password}/g" m2/settings.xml
 cp $(pwd)/m2/settings.xml ~/.m2/settings.xml
