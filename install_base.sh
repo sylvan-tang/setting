66a755a2a1304a3dadd062501abbc3ec6bff4505
sylvan <sylvan@sylvans-Mac-mini.local>
2018-02-11 08:40:32 +0800

    Seperate install commands

---
+++ install_base.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+
+brew install sbt@0.13
+
+brew install apache-spark
+
+brew install maven
+
+brew install snappy
+
+brew install gcc@5
+brew install gcc
+
+brew install lzop
+
+brew install tmux
+
--- install_for_work.sh
+++ install_for_work.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
 
-brew install git
-
-brew install sbt@0.13
-
-brew install apache-spark
-
-brew install maven
-
-brew install snappy
-
 sudo easy_install pip
 
 brew tap zhihu/tools git@git.in.zhihu.com:infrastructure/homebrew-tools.git
 brew install zhihu/tools/zhihu-buildout
-
-brew install gcc@5
-brew install gcc
-
-brew install lzop
-
-brew install tmux
 
 sudo pip install python-gflags==2.0.0 --user -U
 
