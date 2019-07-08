2a62de33cc9b69e5b1f075b8010ba1f406e6b83b
chensuna <chensuna@baidu.com>
2019-07-08 21:32:23 +0800

    Delete environment for work

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,11 @@ cp $(pwd)/git/.gitconfig ~/.gitconfig
 sed -i '' "s/UserName/${user_name}/g" ~/.gitconfig
 sed -i '' "s/YourEmailAddress/${email}/g" ~/.gitconfig
 
-cp $(pwd)/m2/settings.xml ~/.m2/settings.xml
-sed -i '' "s/UserName/${user_name}/g" ~/.m2/settings.xml
-sed -i '' "s/Password/${password}/g" ~/.m2/settings.xml
-
 ln -sf $(pwd)/config/bash_aliases ~/.bash_aliases
 ln -sf $(pwd)/config/bashrc ~/.bashrc
 ln -sf $(pwd)/config/profile ~/.profile
 ln -sf $(pwd)/config/tmux-open-session.sh ~/.tmux-open-session.sh
 
 mkdir ~/.sbt
-ln -sf $(pwd)/sbt/repositories ~/.sbt/repositories
 
 echo "If you are init your MacBook, please change your root password by 'sudo passwd root'"
