cec1b34468b919f4f2f7cdadc45c0e431f4ff281
ru.sylvan <chens.suna@immomo.com>
2019-11-18 10:15:49 +0800

    Use zsh instead of bash

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,11 @@ cp "$(pwd)/git/.gitconfig" ~/.gitconfig
 sed -i '' "s/UserName/${user_name}/g" ~/.gitconfig
 sed -i '' "s/YourEmailAddress/${email}/g" ~/.gitconfig
 
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+echo "" >> ~/.zshrc
+cat "$(pwd)/config/profile" >> ~/.zshrc
 ln -sf "$(pwd)/config/bash_aliases" ~/.bash_aliases
-ln -sf "$(pwd)/config/bashrc" ~/.bashrc
 ln -sf "$(pwd)/config/profile" ~/.profile
 ln -sf "$(pwd)/config/tmux-open-session.sh" ~/.tmux-open-session.sh
 ln -sf "$(pwd)/config/start-for-work.sh" ~/.start-for-work.sh
