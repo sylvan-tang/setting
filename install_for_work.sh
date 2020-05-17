483ee406f1baa78899f3f6dcf057750d6f44bb20
ru.sylvan <chen.suna@immomo.com>
2020-05-17 15:16:18 +0800

    Add config

--- install_for_work.sh
+++ install_for_work.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #brew install sbt@0.13
 #brew install apache-spark
-#brew install maven
+brew install maven
 brew tap AdoptOpenJDK/openjdk
 brew cask install adoptopenjdk11
-brew install gradle
 brew info mysql@5.7
 brew install mysql@5.7
 brew link mysql@5.7 --force
 brew install redis
+brew install docker-compose
