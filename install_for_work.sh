a07d66faab49688be97bf460afaba21535654d7a
sylvan <1176995880@qq.com>
2020-11-03 16:45:22 +0800

    Feature: use java8 instead of java11

--- install_for_work.sh
+++ install_for_work.sh
@@ -3,9 +3,5 @@
 #brew install apache-spark
 brew install maven
 brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk11
-brew info mysql@5.7
-brew install mysql@5.7
-brew link mysql@5.7 --force
-brew install redis
+brew cask install adoptopenjdk8
 brew install docker-compose
