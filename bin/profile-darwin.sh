427526c48b7db5642ca32b871c94f45f714fd7cb
sylvan <sylvan2future@gmail.com>
2021-11-25 14:10:15 +0800

    Feature: 添加 go path 到系统配置中

--- bin/profile-darwin.sh
+++ bin/profile-darwin.sh
@@ -1,1 +1,6 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "/usr/local/go/bin" ] ; then
+    export PATH="/usr/local/go/bin:$PATH"
+fi
