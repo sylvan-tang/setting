8f930e542e3f97e6c0510d0394b95369dbbdbee3
sylvan <sylvan2future@gmail.com>
2022-11-17 19:11:15 +0800

    Feature: install wget

--- bin/install-tools-darwin.sh
+++ bin/install-tools-darwin.sh
@@ -1,64 +1,152 @@
-#!/usr/bin/env bash
-set -x
+#!/usr/bin/:env bash
+
+cat /etc/hosts | grep raw.githubusercontent.com
+if [[ $? != 0 ]] ; then
+  echo "'199.232.96.133  raw.githubusercontent.com' should add into /etc/hosts"
+  exit 1
+fi
+cat /etc/hosts | grep kubernetes.docker.internal
+if [[ $? != 0 ]] ; then
+  echo "'127.0.0.1       kubernetes.docker.internal' should add into /etc/hosts"
+  exit 1
+fi
 
 which brew
 if [[ $? != 0 ]] ; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  while true; do
+    echo "install brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 brew install bash-completion
 
+which wget
+if [[ $? != 0 ]] ; then
+  while true; do
+    echo "install wget..."
+    brew install wget
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
+fi
+
 which git
 if [[ $? != 0 ]] ; then
-  brew install git
+  while true; do
+    echo "install git..."
+    brew install git
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which tmux
 if [[ $? != 0 ]] ; then
-  brew install tmux
+  while true; do
+    echo "install tmux..."
+    brew install tmux
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
+
 
 which ipython
 if [[ $? != 0 ]] ; then
-  brew install ipython
+  while true; do
+    echo "install ipython..."
+    brew install ipython
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 mvn -v
 if [[ $? != 0 ]] ; then
-  brew install maven
+  while true; do
+    echo "install maven..."
+    brew install maven
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which java
 if [[ $? != 0 ]] ; then
-  brew tap AdoptOpenJDK/openjdk
-  brew cask install adoptopenjdk11
+  while true; do
+    echo "install java 11..."
+    brew tap AdoptOpenJDK/openjdk
+    brew cask install adoptopenjdk11
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which docker-compose
 if [[ $? != 0 ]] ; then
-  brew install docker-compose
+  while true; do
+    echo "install docker-compose..."
+    brew install docker-compose
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which zsh
 if [[ $? != 0 ]] ; then
-  brew install zsh
-  chsh -s /bin/zsh root
+  while true; do
+    echo "install zsh..."
+    brew install zsh
+    chsh -s /bin/zsh root
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which groovy
 if [[ $? != 0 ]] ; then
-  brew install groovy
-  sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+  while true; do
+    echo "install groovy..."
+    brew install groovy
+    sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which pkg-config
 if [[ $? != 0 ]] ; then
-  brew install pkg-config
+  while true; do
+    echo "install pkg-config..."
+    brew install pkg-config
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
 
 which rustup
 if [[ $? != 0 ]] ; then
-  curl https://sh.rustup.rs -sSf | sh
-  source "$HOME/.cargo/env"
-  rustup component add rustfmt
+  while true; do
+    echo "install rustup..."
+    curl https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+    rustup component add rustfmt
+    if [[ $? = 0 ]] ; then
+      break
+    fi
+  done
 fi
