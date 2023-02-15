ba74743e25882b83ccc95cb3617279456f68d4cb
sylvan <sylvan2future@gmail.com>
2023-02-15 11:25:44 +0800

    Feature: add mysql-client to system PATH

--- bin/profile-darwin.sh
+++ bin/profile-darwin.sh
@@ -4,5 +4,9 @@ if [ -d "/Library/Frameworks/Python.framework/Versions/3.8/bin" ]; then
 	export PATH="/Library/Frameworks/Python.framework/Versions/3.8/bin:$PATH"
 fi
 
+if [ -d "/usr/local/opt/mysql-client/bin" ]; then
+	export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+fi
+
 # delete gf alias in git
 sed -i '' "s/alias gf='git fetch'//g" ~/.oh-my-zsh/plugins/git/git.plugin.zsh
